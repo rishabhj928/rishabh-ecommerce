7d63aeb070bfc801c507dbf8e771de7b8ffbd832
rishabhj928 <37924781+rishabhj928@users.noreply.github.com>
2022-02-02 23:07:39 +0530

    Delete backend directory

--- backend/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var usersController = require("../controllers/usersController");
-
-router.post("/signup", usersController.createUser);
-
-router.post("/signin", usersController.validateUser);
-
-module.exports = router;
